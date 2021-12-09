f4a198eac0bdaac9fe2cf3844a7fb82b77752c9a
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-12-09 01:02:34 -0300

    adicionado rota inicial

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./local_assets/css/form-2.css"
+const axios = require('axios');
+
 
 const Login = () => {
+  
     return (
         <div className="form-container outer">
             <div className="form-form">
@@ -12,13 +15,13 @@ const Login = () => {
 
                             <h1>Login</h1>
 
-                            <form className="text-left">
+                            <form className="text-left" onSubmit={submit}>
                                 <div className="form">
 
                                     <div id="username-field" className="field-wrapper input">
                                         <label htmlFor="username">NOME DE USUÁRIO</label>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                                        <input id="username" name="username" type="text" className="form-control" placeholder="Nome de usuário" />
+                                        <input id="username" name="username" type="text"  className="form-control" placeholder="Nome de usuário" />
                                     </div>
 
                                     <div id="password-field" className="field-wrapper input mb-2">
@@ -29,14 +32,14 @@ const Login = () => {
 
                                         </div>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
-                                        <input id="password" name="password" type="password" className="form-control" placeholder="Senha" />
+                                        <input id="password" name="password" type="password" className="form-control" placeholder="Senha"  />
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" id="toggle-password" className="feather feather-eye"></svg>
                                     </div>
                                     <div className="d-sm-flex justify-content-between">
                                         <div className="field-wrapper">
-                                            <Link to="/">
+                                            {/* <Link to="/"> */}
                                                 <button type="submit" className="btn btn-primary" value="">Entrar</button>
-                                            </Link>
+                                            {/* </Link> */}
                                             
                                         </div>
                                     </div>
@@ -56,5 +59,31 @@ const Login = () => {
     )
 
 }
+
+ async function submit(event){
+   const api = axios.create({
+        baseURL: 'http://localhost:3200',
+      });
+var email = document.getElementById("username").value;
+var senha = document.getElementById("password").value;
+
+
+  await  api.post('/users/login', 
+    {
+	
+        email: email,
+        senha: senha
+        
+    }
+      )
+      .then(function (response) {
+      var data = response.data;
+        alert("Nome:"+data.usuario.usu_nome+" token:"+data.token);
+      }).catch((error)=>{
+        alert(error);
+      })
+}
+
+
 
 export default Login
